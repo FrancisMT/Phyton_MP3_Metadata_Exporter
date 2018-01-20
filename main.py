e7b63fa8811ff46ae805a22e33e13b6f0f3a9b99
Francisco de Babo Martins <x2francisco@gmail.com>
2018-01-20 23:02:52 +0000

    Each song's metadata is now process in parallel
    Used a ProcessPoolExecutor in order to use one process per song to aquire it's metadata and then add it to a queque which will later on be processed.

--- main.py
+++ main.py
@@ -1,45 +1,54 @@
 import eyed3
+from queue import Queue
 from os import path
-from tkinter import filedialog
+from tkinter import filedialog, simpledialog
+from concurrent.futures import ProcessPoolExecutor
+
+# Queue where all the songs metadata will be placed for later processing
+song_mt_queue = Queue()
 
 
-# Python Threading Tutorial https://stackoverflow.com/questions/16199793/simple-threading-event-example
-class SongData:
-    """Store Song Data and Metada Information"""
+class SongMetaData:
+    """Store Metadata"""
 
     __path = ""
 
-    __title = ""
-    __artist = ""
-    __album = ""
-    __year = ""
-    __genre = ""
+    title = ""
+    artist = ""
+    album = ""
+    year = ""
+    genre = ""
 
     def __init__(self, song_file_path):
         self.__path = song_file_path
-        self.__process_song_metadata()
+        self.__get_song_metadata()
 
-    def __process_song_metadata(self):
+    def __get_song_metadata(self):
         audio_file = eyed3.load(self.__path)
 
-        self.__title = audio_file.tag.title
-        self.__artist = audio_file.tag.artist
-        self.__album = audio_file.tag.album
-        self.__genre = audio_file.tag.genre
-        self.__year = audio_file.tag.best_release_date
+        self.title = audio_file.tag.title
+        self.artist = audio_file.tag.artist
+        self.album = audio_file.tag.album
+        self.genre = audio_file.tag.genre
+        self.year = audio_file.tag.best_release_date
 
-    def print_song_metadata(self):
+    def DEBUG_print_song_metadata(self):
         print("Title: {0}\n"
               "Artist: {1}\n"
               "Album: {2}\n"
               "Genre: {3}\n"
               "Year: {4}\n"
               .format
-              (self.__title,
-               self.__artist,
-               self.__album,
-               self.__genre,
-               self.__year))
+              (self.title,
+               self.artist,
+               self.album,
+               self.genre,
+               self.year))
+
+
+def add_metadata_to_queue(song_file_path):
+    song_metadata = SongMetaData(song_file_path)
+    song_mt_queue.put(song_metadata)
 
 
 def main():
@@ -48,10 +57,18 @@ def main():
     song_paths = filedialog.askopenfilenames(initialdir=current_path, title="Select file",
                                              filetypes=[("mp3 files", "*.mp3")])
 
-    for song_path in song_paths:
-        new_song_data = SongData(song_path)
-        new_song_data.print_song_metadata()
+    # Use ProcessPool instead of ThreadPool to get the each song's metadata in parallel.
+    # Notes {
+    #           * ThreadPool uses one core while  uses multiple cores.
+    #           * Rule of thumb:
+    #               + IO bound jobs -> ThreadPool
+    #               + CPU bound jobs -> ProcessPool
+    #       }
+    with ProcessPoolExecutor() as executor:
+        for song_path in song_paths:
+            executor.submit(add_metadata_to_queue, song_path)
 
+    # Process queue and get .csv file name from user
 
 if __name__ == '__main__':
     main()
