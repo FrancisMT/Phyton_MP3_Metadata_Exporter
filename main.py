c0fdd74fe8fe2a1d564f03ef635d304c82e3e7fa
Francisco de Babo Martins <x2francisco@gmail.com>
2018-01-22 10:21:40 +0000

    .cvs file name for metada exportation is obtained through user input
    Pretty self explanatory.

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import eyed3
+from time import time
 from queue import Queue
 from os import path
 from tkinter import filedialog, simpledialog
-from concurrent.futures import ProcessPoolExecutor
+from concurrent.futures import ProcessPoolExecutor, ThreadPoolExecutor
 
 # Queue where all the songs metadata will be placed for later processing
 song_mt_queue = Queue()
@@ -32,7 +33,7 @@ class SongMetaData:
         self.genre = audio_file.tag.genre
         self.year = audio_file.tag.best_release_date
 
-    def DEBUG_print_song_metadata(self):
+    def debug_print_song_metadata(self):
         print("Title: {0}\n"
               "Artist: {1}\n"
               "Album: {2}\n"
@@ -51,11 +52,19 @@ def add_metadata_to_queue(song_file_path):
     song_mt_queue.put(song_metadata)
 
 
+def process_csv_filename(csv_file_name):
+    if not csv_file_name.endswith(".csv"):
+        csv_file_name += ".csv"
+        return csv_file_name
+
+
 def main():
     current_path = path.dirname(path.realpath(__file__))
 
     song_paths = filedialog.askopenfilenames(initialdir=current_path, title="Select file",
                                              filetypes=[("mp3 files", "*.mp3")])
+
+    start_time = time()
 
     # Use ProcessPool instead of ThreadPool to get the each song's metadata in parallel.
     # Notes {
@@ -64,11 +73,21 @@ def main():
     #               + IO bound jobs -> ThreadPool
     #               + CPU bound jobs -> ProcessPool
     #       }
+
+    # TODO: Figure out why queues work ThrePools but not with ProcessPool
     with ProcessPoolExecutor() as executor:
         for song_path in song_paths:
             executor.submit(add_metadata_to_queue, song_path)
 
-    # Process queue and get .csv file name from user
+    #  Get .csv file name from user and process queue
+    csv_filename = process_csv_filename(simpledialog.askstring("Metadata exporter", ".csv filename: "))
+    while not song_mt_queue.empty():
+        metadata = song_mt_queue.get()
+        metadata.debug_print_song_metadata()
+
+    end_time = time()
+    # print("Finished exporting {0} song(s) to {1}")
+
 
 if __name__ == '__main__':
     main()
